09710a23330815acb7e89f2b5201d8e78417e13e
choijungwoo777 <choijungwoo1004@outlook.kr>
2022-02-06 15:45:00 +0900

    오늘 공붉

--- 창의력 문제/main.cpp
+++ 창의력 문제/main.cpp
@@ -65,10 +65,166 @@ int main() {
 
 
 
+/*
+int main() {
+	char a[200], b[200];
+	int i, p=0;
+	gets(a);
+	
+	for(i=0; a[i] != '\0'; i++){
+		if(a[i] != ' '){
+			if(a[i]>=65 && a[i]<=90){
+				b[p++]=a[i]+32; 
+			}
+			else b[p++]=a[i];
+		}
+	}
+	b[p]='\0';
+	printf("%s\n", b);
+	
+	return 0;	
+}
+*/
 
 
 
 
+
+/*
+int main(){
+	int cnt = 0;
+	char a[30];
+	scanf("%s",&a);
+	for(int i = 0; i < 30; i++){
+		if(a[i] == 40) cnt++;
+		if(a[i] == 41)
+		if((--cnt) < 0) break;
+	}
+	if(cnt==0) {
+		printf("YES");
+	}
+	else printf("NO");
+	return 0;
+}
+*/
+
+
+
+
+
+/*
+int main(){
+	int a, cnt;
+	scanf("%d",&a);
+	for(int i = 1; i <= a; i++)	{
+		cnt = 0;
+		for(int j = 1; j <= i; j++)
+		if(i%j==0) cnt++;
+		printf("%d ",cnt);
+}
+return 0;
+}
+*/
+
+
+
+
+
+/*
+int dight_sum(int x){
+	int tmp, sum=0;
+	while(x>0){
+		tmp=x%10;
+		sum+=tmp;
+		x=x/10;
+	}
+	return sum;
+}
+
+int main() {
+	int n, num, i, sum, max=-2147000000, res;
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%d", &num);
+		sum = dight_sum(num);
+		if(sum>max){
+			max=sum;
+			res=num;
+		}
+		else if(sum==max){
+			if(num>res) res=num;
+		}
+	}
+	printf("%d", res);
+	return 0;
+}
+*/
+
+
+
+
+/*
+int main(){
+	int n, i, cnt=0, tmp;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		tmp=i;
+		while(tmp>0){
+			tmp=tmp/10;
+			cnt++;
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
+*/
+
+
+
+
+
+/*
+int main() {
+	int n, sum=0, cnt=1, d=9, res=0;
+	scanf("%d", &n);
+	while(sum+d<n){
+		res=res+(cnt*d);
+		sum=sum+d;
+		cnt++;
+		d=d*10;
+	}
+	res=res+((n-sum)*cnt);
+	printf("%d\n", res);
+	return 0;
+}
+*/
+
+
+
+
+/*
+int ch[10];
+int main() {
+	int i, digit, max=-2147000000, res;
+	char a[200];
+	scanf("%s", &a);
+	
+	for(i=0; a[i] != '\0'; i++){
+		digit=a[i]-48;
+		ch[digit]++;
+	}
+	
+	for(i=0; i<=9; i++){
+		if(ch[i]>max){
+			max=ch[i];
+			res=i;
+		}
+	}
+	printf("%d\n", res);
+	
+	return 0;
+}
+*/
 
 
 
